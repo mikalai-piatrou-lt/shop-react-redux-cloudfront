d3de00b5a6e3e7435632aee6342f6bd21c621b8c
Mikalai_Piatrou <piatrou.mikalai@gmail.com>
2025-05-30 16:21:58 +0300

    Updated the frontend configuration to integrate the new Lambda endpoint under the import API path.

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://gczr66mvme.execute-api.eu-north-1.amazonaws.com/prod",
   bff: "https://th8y9kpitl.execute-api.eu-north-1.amazonaws.com/prod",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
