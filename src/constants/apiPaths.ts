ff209a515958f89bc3f8b22de790aba50f94462e
Mikalai_Piatrou <piatrou.mikalai@gmail.com>
2025-06-16 16:13:59 +0300

    Changed URL for cart service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://gczr66mvme.execute-api.eu-north-1.amazonaws.com/prod",
   bff: "https://th8y9kpitl.execute-api.eu-north-1.amazonaws.com/prod",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://e6p2t76cq2.execute-api.ap-south-1.amazonaws.com/prod/api",
 };
 
 export default API_PATHS;
